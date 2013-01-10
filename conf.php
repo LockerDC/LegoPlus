c733b470f025e30388471d76bdec3966271cb1c8
Xsikor <Xsikor@gmail.com>
2013-01-10 18:20:02 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,6 +1,6 @@
 <?php
 $db_login = "root"; //Логин к базе
-$db_pass = "123258"; //Пароль к БД
+$db_pass = "root"; //Пароль к БД
 $db_name = "legocms"; //Имя базы
 $db_host = "localhost"; //Где находиться БД
 ?>
